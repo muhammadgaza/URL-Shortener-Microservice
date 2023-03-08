d67335c6a000169e1dd67340f4d1e13e29f56971
muhammadgaza <muhammadgazaalfarizi@smkwikrama.sch.id>
2023-03-08 14:53:30 +0700

    URL-Shortener-Microservice

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extends: true }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +20,53 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// {
+//   "original_url": "https://google.com",
+//     "short_url": 40
+// }
+
+const originalUrls = []
+const shortUrls = []
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+
+  if (!url.includes("https://") && !url.includes("https://")) {
+    return res.json({ error: 'invalid url'})
+  }
+
+  if (foundIndex < 0) {
+    originalUrls.push(url)
+    shortUrls.push(shortUrls.length)
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1
+    })
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  })
+})
+
+app.get("/api/shorturl/:shorturl", (req,res) => {
+  const shorturl = parseInt(req.params.shorturl)
+  const foundIndex = shortUrls.indexOf(shorturl)
+
+  if (foundIndex < 0) {
+    return res.json({
+      "error": "No short URL dfound for the given input"
+    })
+  }
+
+  res.redirect(originalUrls[foundIndex])
+})
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
